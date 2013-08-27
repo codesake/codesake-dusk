8d239f31eb7e360a773839e84265c0817d0100b5
Paolo Perego <thesp0nge@gmail.com>
2013-08-27 12:15:12 +0200

    first commit

---
+++ lib/codesake/dusk.rb
@@ -0,0 +1,7 @@
+require "codesake/dusk/version"
+
+module Codesake
+  module Dusk
+    # Your code goes here...
+  end
+end
---
+++ lib/codesake/dusk/version.rb
@@ -0,0 +1,5 @@
+module Codesake
+  module Dusk
+    VERSION = "0.0.1"
+  end
+end
